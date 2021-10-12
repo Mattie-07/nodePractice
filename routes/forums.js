3050ced81d7fbd5116b439d0536b0533f5a292c5
Matthew <Matthewtroberts10@gmail.com>
2021-10-12 00:27:50 -0500

    minor changes

--- routes/forums.js
+++ routes/forums.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-
 router.use(express.urlencoded({extended:false}));
 router.use(express.json());
+let photoRef = require("../data/data.json")
+let refToPhotos = photoRef.artwork;
 
 router.get("/forums", (req, res) => {
-    res.render('forums')
+    let photos = [];
+    refToPhotos.forEach(element =>{
+        photos = photos.concat(element.art);
+    })
+    res.render('forums',{
+        photo:photos
+    })
 });
-
 
 router.post('/forums', (req, res) => {
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 let dataFile = require("../data/data.json")
-
 let data = dataFile.albums;
+
 router.get("/", (req, res) => {                                         
     let photos = [];                                                //creating an array
     data.forEach(element =>{                                        //using a forEach to initalize the array
